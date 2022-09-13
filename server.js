71998b66ecbb5bbef67b0b2a302ff77b648244ef
faizamahmoud <faiza.mahmoud@email.com>
2022-09-13 10:10:22 -0600

    Home route renders 'Hello World'

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ const app = express();
 
 // set up middleware
 app.use(cors());
+
+
+//home route for testing our app
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+  
+
+  //declare a variable for our port number
+const PORT = process.env.PORT || 8000;
+
+// turn on the server listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
