1e1c1cbf30bbc4af26624309b1eae337ac75ba49
faizamahmoud <faiza.mahmoud@email.com>
2022-09-13 09:57:51 -0600

    imported dependies and JSON files

---
+++ server.js
@@ -0,0 +1,7 @@
+// Import Dependencies
+const express = require("express");
+const cors = require("cors");
+
+// Import JSON files
+const projects = require("./projects.json");
+const about = require("./about.json");
