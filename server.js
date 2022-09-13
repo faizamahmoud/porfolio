32ee43d918e93a71c2e9e85c6f048f60d93a2a8d
faizamahmoud <faiza.mahmoud@email.com>
2022-09-13 10:00:28 -0600

    created app oject and cors middleware

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const cors = require("cors");
 // Import JSON files
 const projects = require("./projects.json");
 const about = require("./about.json");
+
+// Create our app object
+const app = express();
+
+// set up middleware
+app.use(cors());
